c74ffc2bbd86b05440392d3aaebcb3e329b14f0f
Anwarul07 <mdanwarul064@gmail.com>
2024-04-08 11:33:26 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log("Welcome to change the currency value");
+
+function maskPassword(pass){
+    let str = ""
+    for (let index = 0; index < pass.length; index++) {
+        str  += "*"
+    }
+    return str
+}
+
+function copyText(txt) {
+    navigator.clipboard.writeText(txt).then(
+        () => {
+          /* clipboard successfully set */
+          document.getElementById("alert").style.display = "inline"
+          setTimeout(() => {
+            document.getElementById("alert").style.display = "none"
+          }, 2000);
+
+        },
+        () => {
+          /* clipboard write failed */
+          alert("Clipboard copying failed")
+        },
+      );
+  }
+
+const deletePassword = (website)=>{
+    let data = localStorage.getItem("passwords")
+    let arr = JSON.parse(data);
+    arrUpdated = arr.filter((e)=>{
+        return e.website != website
+    })
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated))
+    alert(`Successfully deleted ${website}'s password`)
+    showPasswords()
+
+}
